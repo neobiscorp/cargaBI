10105f377f1e10998700ba0aad20ad3dc1f51447
neobiscorp <operaciones@neobiscorp.com>
2017-09-22 16:59:26 -0300

    @pj ajustes para que la comunicacion con BI sea mas amigable

--- pj_igm_db.R
+++ pj_igm_db.R
@@ -6,7 +6,13 @@
   library(DBI)        #Primary MySQL connection
   library(openxlsx)   #Read xlsx files
   library(data.table) #For merge, rbind and dataframe works
-  
+  killDbConnections <- function () {
+    all_cons <- dbListConnections(MySQL())
+    print(all_cons)
+    for (con in all_cons)
+      +  dbDisconnect(con)
+    print(paste(length(all_cons), " connections killed."))
+  }
   DB <- dbConnect(
     MySQL(),
     user = "root",
@@ -46,16 +52,23 @@
   UAA_users2$Acceso<-NULL
   UAA_users2$IMEI<-NULL
   UAA_users2$REFNUM<-NULL
+  UAA_users2[,'Usuario']<- UAA_users2$Nombre
+  UAA_users2$Nombre<-NULL
+  UAA_users2[,'Acceso']<- UAA_users2$Acceso.x
+  UAA_users2$Acceso.x<-NULL
+  UAA_users2[,'Proveedor']<- UAA_users2$Proveedor.x
+  UAA_users2$Proveedor.x<-NULL
   
   
   dbWriteTable(
     DB,
-    "UAA_users",
+    "usos",
     UAA_users2,
     field.types = list(
       
-      `Acceso.x` = "varchar(255)",
-      `Proveedor.x` = "varchar(255)",
+      `Acceso` = "varchar(255)",
+      `Usuario` = "varchar(255)",
+      `Proveedor` = "varchar(255)",
       `Total (CLP)` = "double(15,2)",
       `Plano tarifario (CLP)` = "double(15,2)",
       `Uso (CLP)` = "double(15,2)",
@@ -66,7 +79,6 @@
       `SMS/MMS (CLP)` = "double(15,2)",
       `Proveedor Nivel 2` = "varchar(255)",
       `Proveedor Nivel 3` = "varchar(255)",
-      `Nombre` = "varchar(255)",
       `Estado` = "varchar(255)",
       `Fecha` = "varchar(255)",
       `Mes` = "INT(10)"
@@ -76,7 +88,7 @@
     append = FALSE,
     allow.keywords = FALSE
   )
-  print("UAA_accesses subido")
+  print("UAA_users subido")
   ######Tabla UTP_accesses######
  UTP_accesses2<-UTP_accesses
  UTP_accesses2$`Acceso fix`<-NULL
@@ -86,16 +98,21 @@
  UTP_accesses2$Proveedor.x<-NULL
  UTP_accesses2$Acceso<-NULL
  UTP_accesses2$Proveedor.y<-NULL
-  
+ UTP_accesses2[,'Acceso']<- UTP_accesses2$Acceso.x
+ UTP_accesses2$Acceso.x<-NULL
+ UTP_accesses2[,'Proveedor']<- UTP_accesses2$Proveedor.x
+ UTP_accesses2$Proveedor.x<-NULL
+ 
+ 
   dbWriteTable(
     DB,
     "UTP_Accesses",
     UTP_accesses2,
     field.types = list(
       
-      `Acceso.x` = "varchar(255)",
+      `Acceso` = "varchar(255)",
       `Producto` = "varchar(255)",
-      `Proveedor.x` = "varchar(255)",
+      `Proveedor` = "varchar(255)",
       `Total (CLP)` = "double(15,2)",
       `Plano tarifario (CLP)` = "double(15,2)",
       `Uso (CLP)` = "double(15,2)",
