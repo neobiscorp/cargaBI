23bebdc9aab20222c365065c4d680004b04a808e
neobiscorp <operaciones@neobiscorp.com>
2017-10-11 08:47:36 -0300

    @pj codigo en R final junto con los comentes

--- pj_igm.R
+++ pj_igm.R
@@ -38,109 +38,8 @@ ACCESSES2[["Tipo"]]<-NULL
                           )
   rm(ASSOCIATIONS2,U_accesses) #Se borran las tablas que no aportan
   
-  
-  # UAA_users <- merge(UA_associations,
-  #                    USERS,
-  #                    by.x = "UUI",
-  #                    by.y = "UUI",
-  #                    all.x = TRUE)
-# DEVICES[,'Null']<-is.na(DEVICES[,'IMEI'])
-#   DEVICES <- subset(DEVICES,
-#                     DEVICES[["Null"]] == FALSE)
-#   DEVICES["Tipo"]<-NULL
-#   DEVICES["REFNUM"]<-NULL
-#   DEVICES["Estado"]<-NULL
-#   DEVICES["Null"]<-NULL
-#   UAAD_users <-merge(UAA_users,
-#                      DEVICES,
-#                      by.x = "IMEI",
-#                      by.y = "IMEI",
-#                      all.x = TRUE)
 
-  
-  # 
-  # uso2 <- uso
-  # a<-duplicated(uso2[["Acceso"]],fromLast = TRUE )
-  # uso2[["duplicado"]] <-a
-  # uso2<-subset(uso2,uso2[["duplicado"]] == FALSE)
-  # PLAN2 <-subset(PLAN,PLAN["Tipo de producto"]=="Plano tarifario")
-  # usounicojoinPLANt <-
-  #   merge(
-  #     uso2,
-  #     PLAN2,
-  #     by.x = "Acceso fix",
-  #     by.y = "Acceso fix",
-  #     all.x = TRUE
-  #   )
-  # usoplantjointipo <-
-  #   merge(
-  #     usounicojoinPLANt,
-  #     TIPO,
-  #     by.x = "Producto",
-  #     by.y = "Producto",
-  #     all.x = TRUE
-  #   )
-  # usoplantjointipo2<-usoplantjointipo
-  # usoplantjointipo2["Proveedor.x"] <- NULL
-  # usoplantjointipo2["Proveedor.y"] <- NULL
-  # 
-  # UTP_accesses<-
-  #   merge (
-  #     usoplantjointipo2,
-  #     ACCESSES,
-  #     by.x = "Acceso fix",
-  #     by.y = "Acceso fix",
-  #     all.x = TRUE
-  #   )
-  # rm(uso2,usounicojoinPLANt,a,PLAN2,usoplantjointipo,usoplantjointipo2)
-
-#Se quiere generar los consumos promedios mensuales por acceso
-# 
-# accesosunicos<-as.list(unique(UTP_accesses[["Acceso.x"]]))
-# i<-2
-# Ptotal<-c()
-# Pplanotarifario<-c()
-# Pusos<-c()
-# Pservicios<-c()
-# Pdescuentos<-c()
-# Pvoz<-c()
-# Pdatos<-c()
-# Psmsmms<-c()
-# nmeses<-c()
-# as.character(accesosunicos[6])
-# 
-# for(i in 1:as.numeric(length(accesosunicos))){
-#   AccMes<-subset(uso,uso[["Acceso"]] == as.character(accesosunicos[[i]])) #Generamos un uso unico al cual le agregamos todos los datos de los distintos meses
-#   nmeses[i]<-as.numeric(length(AccMes[["Acceso"]])) #se ve cuantos meses ha sido ocupado el acceso correspondiente
-#   #Se obtienen los datos necesarios desde AccMes que contiene los mismos titulos de uso junto con los que se agregaron de 557 al 562
-#   Ptotal[i]<-sum(AccMes[["Total (CLP)"]])/nmeses[[i]]
-#   Pplanotarifario[i]<- sum(AccMes[["Plano tarifario (CLP)"]])/nmeses[[i]]
-#   Pusos[i]<-sum(AccMes[["Uso (CLP)"]])/nmeses[[i]]
-#   Pservicios[i]<-sum(AccMes[["Servicios (CLP)"]])/nmeses[[i]]
-#   Pdescuentos[i]<-sum(AccMes[["Descuentos (CLP)"]])/nmeses[[i]]
-#   Pvoz[i]<-sum(AccMes[["Voz (CLP)"]])/nmeses[[i]]
-#   Pdatos[i]<-sum(AccMes[["Datos (CLP)"]])/nmeses[[i]]
-#   Psmsmms[i]<-sum(AccMes[["SMS/MMS (CLP)"]])/nmeses[[i]]
-# }
-# Acceso<-unique(UTP_accesses[["Acceso.x"]])
-# accesosunicos<-as.data.frame(Acceso)
-# #Se unen los datos recopilados a la nueva tabla accesosunicos
-# accesosunicos["Ptotal"]<-Ptotal
-# accesosunicos["Pplanotarifario"]<-Pplanotarifario
-# accesosunicos["Pusos"]<-Pusos
-# accesosunicos["Pservicios"]<-Pservicios
-# accesosunicos["Pdescuentos"]<-Pdescuentos
-# accesosunicos["Pvoz"]<-Pvoz
-# accesosunicos["Pdatos"]<-Pdatos
-# accesosunicos["Psmsmms"]<-Psmsmms
-# accesosunicos["Cantidad Meses"]<-nmeses
-# accesosunicos<-accesosunicos[order(-accesosunicos[["Ptotal"]]),]
-# 
-# rm(Pvoz,Pusos,Ptotal,Psmsmms,Pservicios,Pplanotarifario,Pdescuentos,Pdatos,nmeses,Acceso,i)
-# print("LISTO")
-
-
-############Anomalias
+#Anomalias pueden ser detectadas si se compara el importe de las opciones descontadas con el plano tarifario rebajado
 #se modifica plan eliminando las columnas innecesarias
   PLAN2 <- PLAN
   PLAN2[["Acceso"]] <- NULL
@@ -169,9 +68,5 @@ ACCESSES2[["Tipo"]]<-NULL
       UAADP_usos[["Proveedor"]] == "Claro CL"
   )
   rm(ACCESSES2, UAAD_users, PLAN2) #Se borran las tablas que no aportan
-
-
-
-
 }
 
