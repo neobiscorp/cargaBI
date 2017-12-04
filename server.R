8b30ac40fbef4c4bbfd14226620b626484d2b368
pbugneobis <30902797+pbugneobis@users.noreply.github.com>
2017-12-04 10:18:11 -0300

    @PB actualización links

--- server.R
+++ server.R
@@ -2091,39 +2091,39 @@ shinyServer(function(input, output, session) {
       
       nombre <<- input$nombre
       if (nombre=="Aguas Andinas"){
-      link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=14b87c7347d134ae0a8a84a9bde4255be&authkey=Ab-4yKE7QBg5097G1OprHaQ"}
+      link<<-"https://cdn.pbrd.co/images/GWEts67.jpg"}
       else if (nombre =="Carabineros de Chile"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1c35c8ab1c41a429389c07e884e040ca2&authkey=Ab0PSPo46qtYDU94ifFMPB8"
+        link<<-"https://cdn.pbrd.co/images/GWEu27P.png"
       }
       else if (nombre =="Claro"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1e613dce9bc4a4671ab408bbca6fa3beb&authkey=Ab6bBHuUbCP_8rL8dOFjDhs"
+        link<<-"https://cdn.pbrd.co/images/GWEuhGn.jpg"
       }
       else if (nombre =="Copec"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1c57bedd6c69f48e091fe4e0e816fc80e&authkey=ASmc9Ircbj5yqX3di51nYO4"
+        link<<-"https://cdn.pbrd.co/images/GWEuxvg.png"
       }
       else if (nombre =="Enap"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=124606522a3044ca7a17d46c2e5026828&authkey=AR8wwSbdcTcjhlnT0ucklUA"
+        link<<-"https://cdn.pbrd.co/images/GWEuMor.gif"
       }
       else if (nombre =="Hogar de Cristo"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=158d4400c454a43479500d15fbf4f4a43&authkey=AVwEEx6zRTUuGhrTxCvjj50"
+        link<<-"https://cdn.pbrd.co/images/GWEuX3b.png"
       }
       else if (nombre =="Nuevo Pudahuel"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1709d2b397dbf44cc811dc1613a23b710&authkey=AYjZ7GMugvEUibRKcLrAvuw"
+        link<<-"https://cdn.pbrd.co/images/GWEvad0.png"
       }
       else if (nombre =="Parque Arauco"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1b110a53917b24f729ef51c2aa6de61ad&authkey=AXux97gNnwiwy4pp3FP2cuY"
+        link<<-"https://cdn.pbrd.co/images/GWEvklk.png"
       }
       else if (nombre =="Rhona"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1d33b6087010841f5b53fbd5cf3ebe755&authkey=AWbKk_eNSR-nB7mimj43BWU"
+        link<<-"https://cdn.pbrd.co/images/GWEvuiJ.png"
       }
       else if (nombre =="SAAM"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1ec4824c380334d28804b71200ef0543a&authkey=AdEpYF88JcJs6RpyKn5TjjQ"
+        link<<-"https://cdn.pbrd.co/images/GWEvFgU.png"
       }
       else if (nombre =="Subsole"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=113e709ea78e0423db1533856df55d846&authkey=ATjB3RtTKoEMNCvNPrhRmNM"
+        link<<-"https://cdn.pbrd.co/images/GWEvOqH.png"
       }
       else if (nombre =="Walmart"){
-        link<<-"https://neobiscorp-my.sharepoint.com/personal/operaciones_neobiscorp_com/_layouts/15/guestaccess.aspx?docid=1389f1fbaf84e4045ba36f75994562ab0&authkey=AdG5V8ZPsu3rxxDNZV9TsKw"
+        link<<-"https://cdn.pbrd.co/images/GWEvY8X.jpg"
       }
       else{print("No se encontro un link Para el nombre seleccionado")}
       #Create table logo_cliente if doesnt exist
